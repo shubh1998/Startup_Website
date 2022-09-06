bcaa08d9de80fa2360c93459945a93c3fc4af9c5
shubh1998 <shubhamgupta7440@gmail.com>
2022-09-06 14:46:35 +0530

    feat: added compiler paths in tsconfig

---
+++ src/containers/Home/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NextPage } from 'next'
+import Vercel from '../../assets/images/vercel.svg'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <h1>Home Page: Namaste Next JS</h1>
+      <Vercel />
+    </>
+  )
+}
+
+export default Home
